4f2385c99fc355a70aa90e3f8b58ab98a5093526
Daniel Herrera Sánchez <daniherr@bancolombia.com>
2024-08-09 09:33:31 -0500

    docs 📓: Se adiciona todos los acuerdos de contribución que tendremos como equipo

---
+++ app_devops/android/app/src/main/kotlin/com/example/app_devops/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_devops
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ app_devops/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:app_devops/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
